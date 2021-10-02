4ce53f4f8c1d0b588420348749144289ca7817e7
aeronut343 <halex211@umn.edu>
2021-10-02 10:04:49 -0500

    Finished Mapping Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -5,25 +5,23 @@ console.log("working");
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
 let map = L.map("mapid", {
     center: [
-        40.7, -94.5
+        37.6213, -122.3790
     ],
-    zoom: 4
+    zoom: 5
 });
 
-// Get data from cities.js
-let cityData = cities;
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(city => console.log(city));
-cityData.forEach(city => L.circleMarker(city.location, {radius: city.population/100000}).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map));
-
-//  Add a marker to the map for Los Angeles, California.
-// let marker = L.marker([34.0522, -118.2437]).addTo(map);
-// L.circleMarker([34.0522, -118.2437], {
-//     radius: 300,
-//     color: "white",
-//     fillcolor: '#ffffa1'
-//  }).addTo(map);
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
