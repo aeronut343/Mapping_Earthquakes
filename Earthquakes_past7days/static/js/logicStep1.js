035d6a55b6195cba919f127c35ad8e8219e9528d
aeronut343 <halex211@umn.edu>
2021-10-03 16:06:05 -0500

    Initial commit

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -30,9 +30,9 @@ let baseLayer = {
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
 let map = L.map("mapid", {
     center: [
-        43.7, -79.3
+        39.5, -98.5
     ],
-    zoom: 11,
+    zoom: 3,
     layers: [streets]
 });
 
@@ -40,10 +40,18 @@ let map = L.map("mapid", {
 // Then we add our 'graymap' tile layer to the map.
 L.control.layers(baseLayer).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/aeronut343/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(data =>
+    L.geoJson(data).addTo(map)
+);
 
-d3.json(torontoHoods).then( data => L.geoJSON(data).addTo(map));
+
+
+
+// // Accessing the Toronto neighborhoods GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/aeronut343/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// d3.json(torontoHoods).then( data => L.geoJSON(data).addTo(map));
 
 // Examples:
 // ---------------------------------------------------------------------------------------------------------------------
